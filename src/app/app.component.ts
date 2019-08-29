059a8ffb8e3d91f05695ba2e32df7bf3f2087df2
miguelorjuela45 <miguel-orjuelar@unilibre.edu.co>
2019-08-28 20:11:22 -0500

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Parcial2808';
+  title = 'Miguel Angel Orjuela Riaño';
 }
